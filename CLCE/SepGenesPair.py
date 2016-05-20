3def0a8425e4a595dcf98a9fa5e35d14f090d828
IL-2 <gavrilov.il2@gmail.com>
2016-05-20 22:53:50 +0300

    Right pairs

--- CLCE/SepGenesPair.py
+++ CLCE/SepGenesPair.py
@@ -29,7 +29,7 @@ def getdatanumgen(flname, numgen):
         print('Error in getdatanumgen !')
     return data
 
-def LearnAndErr(data_learn, features, data_1, data_2):
+def LearnAndErr(data_learn, features):
     svm = LinearSVC(C = 1)
     svm.fit(data_learn, features)
     
@@ -44,30 +44,34 @@ def SepGen(numgen, data_1, data_2):
     print('Find pairs of separated genes...')
     Sep_gens = []
     errors = []
-    for i in range(len(numgen)):
-        for k in range(len(numgen)):
+
+    for num_g1 in range(len(numgen)):
+        for num_g2 in range(num_g1, len(numgen)):
             data_learn = []
             features = []
-            for j in range(len(data_1[0])):
-                for l in range(len(data_1[0])):
-                    data_learn.append([data_1[i][j], data_1[k][l]])
-                    features.append(1)
-            for h in range(len(data_2[0])):
-                for g in range(len(data_2[0])):
-                    data_learn.append([data_2[i][h], data_2[k][g]])
-                    features.append(-1)
-            err = LearnAndErr(data_learn, features, data_1, data_2)
+
+            for num_samp in range(len(data_1[0])):
+                data_learn.append((data_1[num_g1][num_samp], data_1[num_g2][num_samp]))
+                features.append(1)
+
+            for num_samp in range(len(data_2[0])):
+                data_learn.append((data_2[num_g1][num_samp], data_2[num_g2][num_samp]))
+                features.append(-1)
+
+            err = LearnAndErr(data_learn, features)
+            errors.append((num_g1, num_g2, err))
             if err == 0:
-                Sep_gens.append([numgen[i], numgen[k]])
-            errors.append([numgen[i], numgen[k], err])
-        print(i)
+                Sep_gens.append(num_g1, num_g2)
+        print('Complete: {}%'.format(num_g1))
+
     return (Sep_gens, errors)
 
 def main():
     flnumgen = 'Separated_Genes'
     numgen = getnumgen(flnumgen)
     numgen.sort()
-    
+    print ("len numgen = " + str(len(numgen)))   
+ 
     fldata_1 = 'small_cell_lung_data.csv'
     data_1 = getdatanumgen(fldata_1, numgen)
 
@@ -77,10 +81,17 @@ def main():
     genes_and_err = SepGen(numgen, data_1, data_2)
     if not len(genes_and_err[0]) == 0:
         fl = open('SepGenesPair.log', 'w')
+        fl.write('Num_gen_1,num_gen_2')
         for i in genes_and_err[0]:
             fl.write(str(i[0]) + ',' + str(i[1]) + '\n')
             print(str(i[0]) + ',' + str(i[1]) + '\n')
     else:
         print('There are not separated pair of genes :(\n')
+
+    f = open('SepGenesPair_Err.log', 'w')
+    f.write('Num_gen_1, num_gen_2, num_err')
+    for i in range(len(genes_and_err[1])):
+        f.write('{},{},{}\n'.format(genes_and_err[1][i][0], genes_and_err[1][i][1], genes_and_err[1][i][2]))
+        print(genes_and_err[1][i])
 
 main()
